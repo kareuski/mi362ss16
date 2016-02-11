2226c2d66fff51d0c4f234edb4f66dba0581c39c
Kirsten Kareusgit config --global user.name Kirsten <kareuski@msu.edu>
2016-02-11 14:51:33 -0500

    updated week1 new heroku app

--- week4/server.js
+++ week4/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 http.createServer( (request, response) => {
-  response.writeHead(200, {'html': 'Hello World\n'});
-  response.end('This is a node app!\n');
+  response.writeHead(200, {'html': 'text/plain'});
+  response.end('Hello World\n':'This is a node app!\n');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
