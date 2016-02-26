47d698914c50392f90292c2ab0cea3d879b24726
Kirsten Kareusgit config --global user.name Kirsten <kareuski@msu.edu>
2016-02-26 11:55:55 -0500

    finished week 4

--- week4/server.js
+++ week4/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer( (request, response) => {
   response.writeHead(200, {'html': 'text/plain'});
-  response.end('Hello World,\n','This is a node app!\n');
+  response.end('<h1>Hello World</h1><p>This is a node app!</p>\n');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
